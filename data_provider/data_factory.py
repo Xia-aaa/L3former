8aef43c4d4bcbb110e6876648d67967f231bcdcf
Xia-aaa <154774201+Xia-aaa@users.noreply.github.com>
2025-06-21 14:36:36 +0800

    Update data_factory.py

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -19,7 +19,7 @@ def data_provider(args, flag):
 
     if flag == 'test':
         shuffle_flag = False
-        drop_last = False
+        drop_last = False   # To ensure fairness
         # batch_size = 1  # bsz=1 for evaluation
         batch_size = args.batch_size  # bsz=1 for evaluation
         freq = args.freq
